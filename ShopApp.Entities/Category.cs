a2d4304de19f715235f5e10192d02716c2a1391b
Recai <recaiozturk54@gmail.com>
2022-06-06 18:23:40 +0300

    Initial commit

---
+++ ShopApp.Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+
+        //çoka çok ilişki olmasayddı bu şekilded yapacaktık
+        //public List<Product> Products { get; set; }
+    }
+}
---
+++ ShopApp.Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopApp.Entities
+{
+    public class Product
+    {
+        public int  Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public List<ProductCategory> ProductCategories { get; set; }
+
+
+        //çoka çok ilişki olmasayddı bu şekilde yapacaktık
+        //public int CategoryId { get; set; }
+        //public Category Category { get; set; }
+    }
+}
