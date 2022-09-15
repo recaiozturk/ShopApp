e0522a7ed66c8f725eef46a4d510780b4d378385
Recai <recaiozturk54@gmail.com>
2022-09-15 16:11:26 +0300

    Added ShopApp.WebApi,Sample ApiController

---
+++ ShopApp.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebApi.Controllers
+{
+    // localhost:4200/api/products
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Products =
+        {
+            "samsung S1",
+            "samsung S2",
+            "samsung S3",
+            "samsung S4"
+        };
+
+        [HttpGet]
+        public string[] GetProducts()
+        {
+            return Products;
+        }
+
+
+        // localhost:4200/api/products/2
+        [HttpGet("{id}")]
+        public string GetProducts(int id)
+        {
+            return Products[id];
+        }
+    }
+}
